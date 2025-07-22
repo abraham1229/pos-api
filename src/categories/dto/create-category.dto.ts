1e6866cdb012fa51966e10eb3f7337524394b4c2
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-07-21 18:31:44 -0600

    feat: add first validation

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from "class-validator"
+import { IsString, IsNotEmpty } from "class-validator"
   
 export class CreateCategoryDto {
   @IsString()
+  @IsNotEmpty({message: "Invalid name"})
   name: string
 }
