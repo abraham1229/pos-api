289a5f93dde92e99e5072eba59e1eac1667d6878
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-07-23 17:43:25 -0600

    feat: finish category crud

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -21,18 +21,21 @@ export class CategoriesService {
 
   async findOne(id: number) {
     const category = await this.categoryRepository.findOneBy({ id })
-    console.log(category)
     if (!category) {
       throw new NotFoundException('Category does not exist')
     }
     return category;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    const category = await this.findOne(id)
+    category.name = updateCategoryDto.name
+    return await this.categoryRepository.save(category)
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async remove(id: number) {
+    const category = await this.findOne(id)
+    await this.categoryRepository.remove(category)
+    return "Category Deleted"
   }
 }
