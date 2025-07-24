e7f770e1c8930745522f6eca4d36d2aae63b5ed8
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-07-24 17:04:58 -0600

    feat: findby id product endpoint

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -57,8 +57,19 @@ export class ProductsService {
 
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.productRepository.findOne({
+      where: { id },
+      relations: {
+        category: true
+      }
+    })
+
+    if (!product) {
+      throw new NotFoundException('Product not found')
+    }
+
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
