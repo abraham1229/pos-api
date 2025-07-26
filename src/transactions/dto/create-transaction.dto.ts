bf2e60f0421051fa6acfcb9999ba36d033b502da
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-07-25 21:11:28 -0600

    feat: dto transactions

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,28 @@
-export class CreateTransactionDto {}
+import { Type } from "class-transformer";
+import { ArrayNotEmpty, IsArray, IsInt, IsNotEmpty, IsNumber, IsOptional, Length, ValidateNested } from "class-validator";
+
+export class TransactionContentsDto {
+  @IsNotEmpty({ message: 'Product ID is required' })
+  @IsInt({ message: 'Invalid product' })
+  productId: number;
+
+  @IsNotEmpty({ message: 'Quantity is required' })
+  @IsInt({ message: 'Invalid quantity' }) // Validate quantity too
+  quantity: number;
+
+  @IsNotEmpty({ message: 'Price is required' })
+  @IsNumber({}, { message: 'Invalid price' })
+  price: number;
+}
+
+export class CreateTransactionDto {
+  @IsNotEmpty({ message: 'Total is required' })
+  @IsNumber({}, { message: 'Invalid total' })
+  total: number
+
+  @IsArray()
+  @ArrayNotEmpty({ message: 'Contents is required' })
+  @ValidateNested()
+  @Type(() => TransactionContentsDto)
+  contents: TransactionContentsDto[]
+}
