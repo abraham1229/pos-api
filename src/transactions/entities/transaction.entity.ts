3d060a6dc6b573fcf1e74d12b629c7474f51fb5e
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-07-25 21:02:16 -0600

    feat: create transaction ans transactioncontents entity

--- src/transactions/entities/transaction.entity.ts
+++ src/transactions/entities/transaction.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Product } from "src/products/entities/product.entity";
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Transaction {
@@ -10,4 +11,25 @@ export class Transaction {
 
   @Column({type: 'timestamp', default: () => "CURRENT_TIMESTAMP(6)"})
   transactionDate: Date
+
+  @OneToMany(() => TransactionContents, (transactionContent) => transactionContent.transaction)
+  contents: TransactionContents[]
+}
+
+@Entity()
+export class TransactionContents {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column('int')
+  quantity: number
+
+  @Column('decimal')
+  price: number
+
+  @ManyToOne(() => Product, (product) => product.id, {eager: true})
+  product: Product
+
+  @ManyToOne(() => Transaction, (transaction) => transaction.contents)
+  transaction: Transaction
 }
