93332054a3214557438d8c090c58d181664da72e
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-07-26 09:08:32 -0600

    feat: coupons dto

--- src/coupons/dto/create-coupon.dto.ts
+++ src/coupons/dto/create-coupon.dto.ts
@@ -1,1 +1,17 @@
-export class CreateCouponDto {}
+import { IsDateString, IsInt, IsNotEmpty, Max, Min } from "class-validator"
+
+export class CreateCouponDto {
+
+  @IsNotEmpty({ message: 'Invalid coupon name' })
+  name: string
+
+  @IsNotEmpty({ message: 'Invalid coupon percentage' })
+  @IsInt({ message: 'Percentage must be an integer' })
+  @Max(100, {message: 'Maximum value is 100'}) 
+  @Min(1, {message: 'Minumum value is 1'}) 
+  percentage: number
+
+  @IsNotEmpty({ message: 'Invalid coupon date' })
+  @IsDateString({}, {message: 'Invalid coupon date'})
+  expirationDate: Date
+}
