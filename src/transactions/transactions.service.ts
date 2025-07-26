3850fd4c88793a2471c070cad5c99103f1f08e47
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-07-25 21:44:26 -0600

    feat: start create service for transactions

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,11 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Transaction, TransactionContents } from './entities/transaction.entity';
+import { Repository } from 'typeorm';
+import { Product } from 'src/products/entities/product.entity';
 
 @Injectable()
 export class TransactionsService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  constructor(
+    @InjectRepository(Transaction) private readonly transactionRepository: Repository<Transaction>,
+    @InjectRepository(TransactionContents) private readonly transactionContentsRepository: Repository<TransactionContents>,
+    @InjectRepository(Product) private readonly productRepository: Repository<Product>
+  ){}
+
+  async create(createTransactionDto: CreateTransactionDto) {
+    const transaction = new Transaction()
+    transaction.total = createTransactionDto.total
+    await this.transactionRepository.save(transaction)
+
+    for (const content of createTransactionDto.contents) {
+      const product = await this.productRepository.findOneBy({ id: content.productId })
+      if (!product) throw new NotFoundException('ProductId not found')
+      
+      if (content.quantity > product.inventory) {
+        throw new BadRequestException(`The product ${product.name} is over the available stock`)
+      }
+        
+      product.inventory -= content.quantity
+
+
+      
+      await this.transactionContentsRepository.save({...content, transaction, product})
+    }
+
+    return "Transaction saved successfully"
   }
 
   findAll() {
